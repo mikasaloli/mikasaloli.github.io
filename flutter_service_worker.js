f0eeea7082baad463c34c111eef49bee6fc463c4
mikasaloli <xokthilat@gmail.com>
2021-07-31 21:45:59 +0700

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4c7ac2b4a47f56ae19e03c493313909a",
-"index.html": "048ac5249681b375ad2658139e80496e",
-"/": "048ac5249681b375ad2658139e80496e",
-"main.dart.js": "8d2cbcc3457d253a4dc6429d5f9ec244",
+"index.html": "af64614421e6c11fc7d684588b582aaf",
+"/": "af64614421e6c11fc7d684588b582aaf",
+"main.dart.js": "8b0bab1f6baccf27e23303328d54e8ce",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
